e85f11a08e2b8dd24d1bd75b3cbf1b640d401989
Brexton Lane <19.brexton.lane@ksd.org>
2019-04-23 09:42:24 -0700

    added title scene new scripts and a font

--- Archology3D/Assets/Scripts/EnemyProjectileRaycast.cs
+++ Archology3D/Assets/Scripts/EnemyProjectileRaycast.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class EnemyProjectileRaycast : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
+    public float chaseTriggerDistance = 3.0f;
+    public GameObject prefab;
+    public float shootSpeed = 10;
+    float timer = 0;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        Vector3 playerPosition = player.transform.position;
+        Vector3 chaseDirection = playerPosition - transform.position;
+        if (chaseDirection.magnitude < chaseTriggerDistance && timer > 2.5f)
+        {
+            timer = 0;
+            Vector3 shootDir = chaseDirection;
+            shootDir.Normalize();
+            Vector3 offset = shootDir;
+            shootDir = shootDir * shootSpeed;
+            //when calculating a vector from a to b
+            //always do destination - start position
+            GameObject Bullet = (GameObject)Instantiate(prefab, transform.position + offset, Quaternion.identity);
+            Bullet.GetComponent<Rigidbody2D>().velocity = shootDir;
+        }
     }
 }
