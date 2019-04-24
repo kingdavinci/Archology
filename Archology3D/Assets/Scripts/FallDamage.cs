6336fde240604eb81de709beb6ad3812738ccc02
Brexton Lane <19.brexton.lane@ksd.org>
2019-04-24 08:12:47 -0700

    fixing merge

---
+++ Archology3D/Assets/Scripts/FallDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDamage : MonoBehaviour
+{
+    private RigidbodyFirstPersonController score = GetComponent<RigidbodyFirstPersonController>();
+    bool Grounded = ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
