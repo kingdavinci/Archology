227e5181ca8ccb80aaf056bfa5b9aae791540ae2
Andrew Nelson <20.andrew.nelson@ksd.org>
2019-04-24 09:05:34 -0700

    Canvas Prefab

--- Archology3D/Assets/Scripts/PlayerHP.cs
+++ Archology3D/Assets/Scripts/PlayerHP.cs
@@ -8,8 +8,8 @@ public class PlayerHP : MonoBehaviour
 {
     public int HP = 10;
     public Text HPText;
-    public Slider HPBar;
-    public GameObject Deathcanvas;
+  //  public Slider HPBar;
+   // public GameObject Deathcanvas;
     float timer = 0;
 
      void Update()
@@ -19,24 +19,23 @@ public class PlayerHP : MonoBehaviour
     void Start()
     {
         HPText.GetComponent<Text>().text = "Health: " + HP;
-        HPBar.GetComponent<Slider>().value = HP;
+      //  HPBar.GetComponent<Slider>().value = HP;
     }
 
    
 
-    void OnCollisionStay2D(Collision2D collision)
+    void OnCollisionStay(Collision collision)
     {
         if (timer > 0.5f && collision.gameObject.tag == "Enemy" || timer > 0.5f && collision.gameObject.tag == "Boss")
             {
             timer = 0;
             HP--;
             HPText.GetComponent<Text>().text = "Health: " + HP;
-            HPBar.GetComponent<Slider>().value = HP;
+           // HPBar.GetComponent<Slider>().value = HP;
             if (HP <= 0)
 
             {
-                Time.timeScale = 0;
-                Deathcanvas.SetActive(true);
+                SceneManager.LoadScene("LoseScene");
             }
         }
     }
