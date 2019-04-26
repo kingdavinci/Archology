b8629b80c343193d22a54f666d328fefa3aba739
Brexton Lane <19.brexton.lane@ksd.org>
2019-04-26 08:10:21 -0700

    just in case to prevent

---
+++ Archology3D/Assets/Scripts/FireExtinguisher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireExtinguisher : MonoBehaviour
+{
+    public GameObject Extinguisher;
+
+    // Update is called once per frame
+    void Update()
+    {
+        void OnCollisionEnter(Collision Collision)
+        {
+            if (Collision.gameObject.tag == "Weapon")
+            {
+                
+            }
+        }
+    }
+}
