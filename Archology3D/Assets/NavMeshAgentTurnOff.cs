5c4b895b74784af044ef6e0d788d703ccd8d4ff8
Kent Melville <19.kent.melville@ksd.org>
2019-05-02 09:01:15 -0700

    created & added to one enenmy, NavMeshTurnOff Script

---
+++ Archology3D/Assets/NavMeshAgentTurnOff.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+[RequireComponent(typeof(EnemyNavMov))]
+public class NavMeshAgentTurnOff : MonoBehaviour
+{
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            agent.enabled = false;
+            GetComponent<EnemyNavMov>().enabled = false;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            //agent.enabled = true;
+            //GetComponent<EnemyNavMov>().enabled = true ;
+        }
+    }
+
+}
