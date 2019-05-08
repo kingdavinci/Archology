55ace09e1dcda263f82d05ca21aaad51cf37b2fd
Andrew Nelson <20.andrew.nelson@ksd.org>
2019-05-08 08:48:23 -0700

    text appear on medkit enter

--- Archology3D/Assets/Prefabs/Scripts/MedKit.cs
+++ Archology3D/Assets/Prefabs/Scripts/MedKit.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MedKit : MonoBehaviour
 {
     public int Health;
     public GameObject Player;
+    public Text Text;
+    private float timer;
+    private bool timerStart = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +18,23 @@ public class MedKit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(timerStart)
+        {
+            timer += Time.deltaTime;
+            if(timer >= 3.5)
+            {
+                Text.enabled = false;
+                timerStart = false;
+                timer = 0;
+            }
+        }
     }
     void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.tag == "MedKit")
         {
+            Text.enabled = true;
+            timerStart = true;
             Player.GetComponent<PlayerHP>().HP += Health;
             Destroy(gameObject);
         }
