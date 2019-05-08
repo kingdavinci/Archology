5c0c5945f646c6ae4293371ebfd6150834c6ae1c
Andrew Nelson <20.andrew.nelson@ksd.org>
2019-05-08 08:19:19 -0700

    medkit script

---
+++ Archology3D/Assets/Prefabs/Scripts/MedKit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedKit : MonoBehaviour
+{
+    public int Health;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "MedKit")
+        {
+            Player.GetComponent<PlayerHP>().HP += Health;
+            if(Player.GetComponent<PlayerHP>().HP > 10)
+            {
+                Player.GetComponent<PlayerHP>().HP = 10;
+            }
+        }
+    }
+}
