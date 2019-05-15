ffd291657b1941a580fc1e3d35618b0c1488dabb
Kent Melville <19.kent.melville@ksd.org>
2019-05-15 09:59:11 -0700

    Made layers for player/fireextinguisher/enemybullets, made 2nd objective by adjusting/moving loadlevel2 script & by adjusting objective text

--- Archology3D/Assets/Prefabs/Scripts/LoadLevel2.cs
+++ Archology3D/Assets/Prefabs/Scripts/LoadLevel2.cs
@@ -5,11 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class LoadLevel2 : MonoBehaviour
 {
+    int EnemyCounter;
+
+    private void Start()
+    {
+        EnemyCounter = GameObject.FindGameObjectsWithTag("Enemy").Length;
+    }
     void OnCollisionEnter(Collision collision)
     {
+        
         //attaching this script to the portal
         //will load the win screen when player collides
-        if (collision.gameObject.tag == "Player" && )
+        if (collision.gameObject.tag == "Flag" && EnemyCounter - GameObject.FindGameObjectsWithTag("Enemy").Length >= 2)
         {
             SceneManager.LoadScene("Level2");
         }
