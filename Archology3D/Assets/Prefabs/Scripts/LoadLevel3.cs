697d90efb08974a58fc1a95cf5ec77cfccf24e38
Andrew Nelson <20.andrew.nelson@ksd.org>
2019-05-16 09:40:10 -0700

    added flag to level 2

---
+++ Archology3D/Assets/Prefabs/Scripts/LoadLevel3.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadLevel3 : MonoBehaviour
+{
+    int EnemyCounter;
+
+    private void Start()
+    {
+        EnemyCounter = GameObject.FindGameObjectsWithTag("Enemy").Length;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+
+        //attaching this script to the portal
+        //will load the win screen when player collides
+        if (collision.gameObject.tag == "Flag" && EnemyCounter - GameObject.FindGameObjectsWithTag("Enemy").Length >= 2)
+        {
+            SceneManager.LoadScene("LevelHardCore");
+        }
+    }
+}
